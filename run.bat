e5dcc949bc416033ad74435a10a1051da50a7689
leomaurodesenv <leo.mauro.desenv@gmail.com>
2020-07-21 14:26:03 -0300

    remove unnecessary files

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 ::-- Variables
-SET APP_IMAGE=data-science-api:0.2.2
-SET APP_NAME=data-science-api-container
+SET APP_IMAGE=ds-api
+SET APP_NAME=ds-api-container
 
 ::-- Introduction message
 TITLE Data Science API Framework
@@ -16,7 +16,7 @@ ECHO -----------------------------------------
 :: build image
 docker build -t %APP_IMAGE% .
 :: running container
-docker run -d --name %APP_NAME% -p 5050:5050 %APP_IMAGE%
+docker run --rm -it --name %APP_NAME% -p 5050:5050 %APP_IMAGE%
 
 ::-- Conclusion
 ECHO -----------------------------------------
