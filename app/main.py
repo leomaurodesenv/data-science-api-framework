d042e9818f2e2b1ed5b8e2741d697b209fb656a3
leomaurodesenv <leo.mauro.desenv@gmail.com>
2020-08-11 17:13:21 -0300

    fix dockerfile, add gunicorn

--- app/main.py
+++ app/main.py
@@ -11,33 +11,30 @@
 import config
 from restplus import api
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 #-- Endpoints
 from endpoints.hello_world import api as ns_hello
 
 
 #-- API predefinitions
-app = Flask(config.FLASK_NAME)
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
 #-- API Configurations
 def configure_app(app):
     app.config['DEBUG'] = config.FLASK_DEBUG
     app.config['ENV'] = config.FLASK_ENV
-    app.config['RESTPLUS_VALIDATE'] = config.RESTPLUS_VALIDATE
-    app.config['ERROR_404_HELP'] = config.RESTPLUS_ERROR_404_HELP
     app.config['SWAGGER_UI_DOC_EXPANSION'] = config.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
     app.config['RESTPLUS_MASK_SWAGGER'] = config.RESTPLUS_MASK_SWAGGER
 
 
 #-- API Init
-def initialize_app(app):
-    configure_app(app)
-    #-- add your endpoints here
-    api.init_app(app)
-    api.add_namespace(ns_hello)
+configure_app(app)
+#-- add your endpoints here
+api.init_app(app)
+api.add_namespace(ns_hello)
 
 
-#-- API run
-if __name__ == "__main__":
-    initialize_app(app)
+if __name__ == '__main__':
     app.run(host=config.FLASK_HOST, port=config.FLASK_PORT)
--- app/restplus.py
+++ app/restplus.py
@@ -5,12 +5,11 @@ from flask_restplus import Api
 
 #-- API global var
 api = Api(version=config.RESTPLUS_VERSION, title=config.RESTPLUS_TITLE,
-    description=config.RESTPLUS_DESCRIPTION,
-)
+    description=config.RESTPLUS_DESCRIPTION)
 
 
 #-- 500
-# @api.errorhandler
-# def default_error_handler(e):
-#     message = 'An unhandled exception occurred.'
-#     return {'message': message}, 500
+@api.errorhandler
+def default_error_handler(e):
+    message = 'An unhandled exception occurred.'
+    return {'message': message}, 500
